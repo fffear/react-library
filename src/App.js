16fd7e4b2b54498ceb64938dabf46a04d7380c47
Kevin Vuong <c_k.k@hotmail.com>
2020-02-17 15:25:29 +1100

    eject to add css modules

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-header" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <header className="App-header">
+      <h1>My Library</h1>
+      <button>Add New Book</button>
+    </header>
+  );
+};
+
+export default header;
